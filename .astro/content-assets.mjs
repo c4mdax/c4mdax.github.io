385e834c306db11ebc81eeff3b16e009dba3b753
Luis Angel <c4mdax@ciencias.unam.mx>
2026-05-26 20:26:49 -0600

    Actualizando favicon

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_ZBGVtY from "../../assets/posts-images/rag-llm.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Frag-structure.md";
-import __ASTRO_IMAGE_IMPORT_ZqoMid from "../../assets/projects-images/atomic-pulse.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fatomicpulse.md";
 import __ASTRO_IMAGE_IMPORT_Zi0JQx from "../../assets/projects-images/dragan-rag-agent.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fdragan-rag.md";
-export default new Map([["../../assets/posts-images/rag-llm.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Frag-structure.md", __ASTRO_IMAGE_IMPORT_ZBGVtY], ["../../assets/projects-images/atomic-pulse.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fatomicpulse.md", __ASTRO_IMAGE_IMPORT_ZqoMid], ["../../assets/projects-images/dragan-rag-agent.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fdragan-rag.md", __ASTRO_IMAGE_IMPORT_Zi0JQx]]);
+import __ASTRO_IMAGE_IMPORT_ZqoMid from "../../assets/projects-images/atomic-pulse.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fatomicpulse.md";
+export default new Map([["../../assets/posts-images/rag-llm.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Frag-structure.md", __ASTRO_IMAGE_IMPORT_ZBGVtY], ["../../assets/projects-images/dragan-rag-agent.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fdragan-rag.md", __ASTRO_IMAGE_IMPORT_Zi0JQx], ["../../assets/projects-images/atomic-pulse.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fatomicpulse.md", __ASTRO_IMAGE_IMPORT_ZqoMid]]);
 		
